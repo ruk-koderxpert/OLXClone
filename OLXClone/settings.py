8be5ac69418a3d078aff67b231bd91d421ce4814
rutvikpatel <rutvikkamani28@gmail.com>
2024-05-03 12:01:30 +0530

    Done

--- OLXClone/settings.py
+++ OLXClone/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE' : 'django.db.backends.mysql',
         'NAME' : 'olx',
-        'USER' : 'root',
-        'PASSWORD' : '',
+        'USER' : 'admin',
+        'PASSWORD' : 'Rutvik@9012',
         'HOST' : 'localhost',
         'PORT' : '3306',
     }
